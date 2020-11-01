f1b2f9de1baf8cfb7e230ddc42f1fe2766946903
elinava-web <69087743+elinava-web@users.noreply.github.com>
2020-11-01 10:54:11 -0700

    Add files via upload

---
+++ miplantilla/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="wrapp">
+        <p> No lista 01 xxxx</p>
+        <p> // SUBMÓDULO 2
+            Desarrolla aplicaciones Web con conexión a bases de datos</p>
+        <p>https://github.com/elinava-web </p>
+    </div>
+</footer>
